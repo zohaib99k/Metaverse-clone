38c1e2ff0166148fd67e96dabe762c64354e08bc
zohaib99k <gg98190@gmail.com>
2022-04-04 13:29:11 +0500

    commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,7 +3,8 @@ import type { AppProps } from 'next/app'
 import { MoralisProvider } from "react-moralis";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return( <MoralisProvider 
+  return(
+     <MoralisProvider 
   appId={process.env.NEXT_PUBLIC_APP_ID}
    serverUrl={process.env.NEXT_PUBLIC_SERVER_URL}>
   <Component {...pageProps} />
