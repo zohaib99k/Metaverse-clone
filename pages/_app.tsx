187695d626b4bf88560e518781f352c941bd6d6d
zohaib99k <gg98190@gmail.com>
2022-04-04 12:33:46 +0500

    rm lock file

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,7 +3,9 @@ import type { AppProps } from 'next/app'
 import { MoralisProvider } from "react-moralis";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return( <MoralisProvider appId={process.env.NEXT_PUBLIC_APP_ID} serverUrl={process.env.NEXT_PUBLIC_SERVER_URL}>
+  return( <MoralisProvider 
+  appId={process.env.NEXT_PUBLIC_APP_ID}
+   serverUrl={process.env.NEXT_PUBLIC_SERVER_URL}>
   <Component {...pageProps} />
 </MoralisProvider>,
   
