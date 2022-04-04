51385a7317fdde81c7e88b5547f1ace4d08147c8
zohaib99k <gg98190@gmail.com>
2022-04-04 21:22:40 +0500

    add: msg components

---
+++ components/Messages.js
@@ -0,0 +1,51 @@
+import React from 'react'
+import { useRef } from 'react'
+
+import { ByMoralis, useMoralis, useMoralisQuery } from 'react-moralis'
+import SendMessage from './SendMessage'
+import massage from './Message'
+import Message from './Message'
+
+const MINS_DURATION = 15
+
+function Messages () {
+  const { user } = useMoralis()
+  const endOfMessagesRef = useRef(null)
+  const { data, error, loading } = useMoralisQuery(
+    'Messages',
+    query =>
+      query
+        .ascending('createdAt')
+        .greaterThan(
+          'createdAt',
+          new Date(Date.now() - 1000 * 60 * MINS_DURATION)
+        ),
+    [],
+    {
+      live: true
+    }
+  )
+
+  return (
+    <div className='pb-56'>
+      <div className='my-5'>
+        <ByMoralis
+          variant='dark'
+          style={{ marginLeft: 'auto', marginRight: 'auto' }}
+        />
+      </div>
+      <div className="space-y-10 p-4">
+        {data.map(message => (
+          <Message key={message.id} message={message} />
+        ))}
+      </div>
+      <div className='flex justify-center'>
+        <SendMessage endOfMessagesRef={endOfMessagesRef} />
+      </div>
+      <div ref={endOfMessagesRef} className='text-center text-gray-400 mt-5'>
+        <p>You're up to date {user.getUsername()}! ❤️‍🔥✨</p>
+      </div>
+    </div>
+  )
+}
+export default Messages
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,24 +1,31 @@
-import type { NextPage } from 'next'
+import { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
 import Login from '../components/Login'
-import { useMoralis } from "react-moralis";
+import { useMoralis } from 'react-moralis'
+import Header from '../components/Header'
+import Messages from '../components/Messages'
 
 const Home: NextPage = () => {
-  const { authenticate, isAuthenticated, user , logout} = useMoralis();
+  const { authenticate, isAuthenticated, user, logout } = useMoralis()
 
   // const isAuthenticated = false;
 
-  if (!isAuthenticated) return <Login/>;
+  if (!isAuthenticated) return <Login />
   return (
-    <div className="h-screen">
+    <div className='h-screen overflow-y-scroll bg-gradient-to-b
+    from-black to-fuchsia-900 overflow-hidden'>
       <Head>
         <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
+        <link rel='icon' href='/favicon.ico' />
       </Head>
-      <h1>hello</h1>
-      <button onClick={logout}>logout</button>
-      {/* <Login /> */}
+      
+        <div className="max-w-screen-2xl mx-auto">
+          <Header/>
+          <Messages/>
+        </div>
+      
+      
     </div>
   )
 }
